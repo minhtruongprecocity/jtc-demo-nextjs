a95a152c9235d8be9b6f1b8ce8a0e63185e0f398
Minh Truong <minhtruong@precocityllc.com>
2023-07-27 11:09:13 -0500

    Load landing page from CMS

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,33 @@
-export default function Home() {
-  return (
-    <div className="container mx-auto p-5">
-      <div className="grid md:grid-cols-2 gap-5 lg:gap-10">
-        <a href="/companies" className="group bg-white inline-block hover:bg-gray-50 shadow-sm">
-          <img
-            src="https://picsum.photos/seed/company-annual/640/480"
-            alt="static page"
-            className="object-cover w-full h-48 md:h-auto"
-          />
-          <div className="text-lg font-bold  p-4">
-            <span className="group-hover:underline text-blue-950">List of Companies</span>
-            <div className="text-sm font-normal">Static</div>
-          </div>
-        </a>
+import { cms } from "@/api/cms";
+import { Container } from "@/components/Container";
+import { Link } from "@/types/Link";
+import Image from "next/image";
 
-        <a href="/books" className="group bg-white inline-block hover:bg-gray-50 shadow-sm">
-          <img
-            src="https://picsum.photos/seed/books/640/480"
-            alt="static page"
-            className="object-cover w-full h-48 md:h-auto"
-          />
-          <div className="text-lg font-bold  p-4">
-            <span className="group-hover:underline text-blue-950">Project Gutenburg Books</span>
-            <div className="text-sm font-normal">Dynamic</div>
-          </div>
-        </a>
+async function getContent() {
+  const content = await cms.items.find("179426159", { nested: true, version: "current" });
+  return content.links as Array<Link>;
+}
+
+export default async function Page() {
+  const links = await getContent();
+
+  return (
+    <Container>
+      <div className="grid md:grid-cols-2 gap-5 lg:gap-10">
+        {links.map((link) => (
+          <a
+            key={link.id}
+            href={link.attributes.url}
+            className="group bg-white inline-block hover:bg-gray-50 shadow-sm"
+          >
+            <img src={link.attributes.image_url} alt="static page" className="object-cover w-full h-48 md:h-auto" />
+            <div className="text-lg font-bold  p-4">
+              <span className="group-hover:underline text-blue-950">{link.attributes.title}</span>
+              <div className="text-sm font-normal">{link.attributes.description}</div>
+            </div>
+          </a>
+        ))}
       </div>
-    </div>
+    </Container>
   );
 }
